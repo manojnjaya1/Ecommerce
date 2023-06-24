e34a0befa8c671e32d32f95865e0f10a8076abc1
Manoj <manojnjaya@gmail.com>
2023-06-24 13:16:47 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ process.on("uncaughtException", (err) => {
 if (process.env.NODE_ENV !== "PRODUCTION") {
  require("dotenv").config({ path: "backend/config/config.env" });
 }
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static("frontend/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 // Connecting to database
 connectDatabase();
 
